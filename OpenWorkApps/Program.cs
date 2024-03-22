8b0ba7687f189ab9e50a99a4d48b31e35f5ef9d1
Eduardo Lima <eduardoolimaprog@gmail.com>
2024-03-22 17:04:01 -0300

    Adicionar arquivos de projeto.

---
+++ OpenWorkApps/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+Process.Start(@"C:\Program Files\WindowsApps\MSTeams_24033.811.2738.2546_x64__8wekyb3d8bbwe\ms-teams.exe");
+Process.Start(@"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe");
+Process.Start(@"C:\Program Files\OpenVPN Connect\OpenVPNConnect.exe");
+Environment.Exit(0);
+
