c3dbcbc404e87ecc59eac8240bd3f73db8beabbe
Eduardo Lima <eduardoolimaprog@gmail.com>
2024-04-26 11:07:28 -0300

    .

--- OpenWorkApps/Program.cs
+++ OpenWorkApps/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System.Diagnostics;
 
 Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
-Process.Start(@"C:\Program Files\WindowsApps\MSTeams_24033.811.2738.2546_x64__8wekyb3d8bbwe\ms-teams.exe");
 Process.Start(@"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe");
 Process.Start(@"C:\Program Files\OpenVPN Connect\OpenVPNConnect.exe");
+Process.Start(@"C:\Users\eduar\AppData\Local\Microsoft\WindowsApps\MSTeams_8wekyb3d8bbwe\ms-teams.exe");
 Environment.Exit(0);
-
